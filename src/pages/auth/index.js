7c2973eb85f6e699d395b98ce7e9cba15fabf6b7
Andrey Orlov <orlov75@gmail.com>
2023-07-26 21:07:30 +0300

    submit on auth & change-password

--- src/pages/auth/index.js
+++ src/pages/auth/index.js
@@ -1,84 +1,113 @@
-import Button from '../../components/button/index';
-import Title from '../../components/title/index';
-import Link from '../../components/link/index';
-import Input from '../../components/inputs/index';
-import Error from '../../components/error';
-import errorMsg from '../../data/errorMsg';
-import validations from '../../helpers/validation';
-import showError from '../../helpers/showError';
+import Button from "../../components/button/index";
+import Title from "../../components/title/index";
+import Link from "../../components/link/index";
+import Input from "../../components/inputs/index";
+import Error from "../../components/error";
+import errorMsg from "../../data/errorMsg";
+import validations from "../../helpers/validation";
+import showError from "../../helpers/showError";
 
 function render(temp, arrBlock) {
     arrBlock.forEach((item) => {
         temp.appendChild(item.getContent());
-    })
+    });
     return temp;
-};
+}
 
-const template = document.getElementById('app');
+const form = document.createElement("form");
+const template = document.getElementById("app").appendChild(form);
 
 const input = new Input({
-    inputType: 'text',
-    inputName: 'login',
-    placeHolderText: 'логин',
-    inputId: 'enterLogin'
+    inputType: "text",
+    inputName: "login",
+    placeHolderText: "логин",
+    inputId: "enterLogin",
 });
 const errorLogin = new Error({
     message: errorMsg.messages.login,
     errorId: "errorLogin",
 });
 const password = new Input({
-    inputType: 'password',
-    inputName: 'password',
-    placeHolderText: 'пароль',
-    inputId: 'enterPassword'
+    inputType: "password",
+    inputName: "password",
+    placeHolderText: "пароль",
+    inputId: "enterPassword",
 });
 const errorPassword = new Error({
     message: errorMsg.messages.password,
     errorId: "errorPassword",
 });
 const link = new Link({
-    url: '/registration',
-    title: 'Зарегистрироваться'
+    url: "/registration",
+    title: "Зарегистрироваться",
 });
 const title = new Title({
-    text: 'Вход'
+    text: "Вход",
 });
 const button = new Button({
     class: "btn",
-    type: 'submit',
-    url: '/chating',
-    label: 'Войти',
-    id: 'enterBtn',
+    type: "submit",
+    // url: '/chating',
+    label: "Войти",
+    id: "enterBtn",
 });
 
-const result = [title, input, errorLogin, password, errorPassword, button, link]
+const result = [
+    title,
+    input,
+    errorLogin,
+    password,
+    errorPassword,
+    button,
+    link,
+];
 
-if (window.location.pathname === '/' || window.location.pathname === '/auth') {
-    render(template, result)
-};
+if (window.location.pathname === "/" || window.location.pathname === "/auth") {
+    render(template, result);
+}
 
 function validator(field, value) {
     if (field === "login") {
         return !validations.validations.login(value);
     } else if (field === "password") {
         return !validations.validations.password(value);
-    } 
-};
+    }
+}
 
 const currentFormAuth = {
-    'login': '',
-    'password': '',
-}
+    login: "",
+    password: "",
+};
 
 template.querySelectorAll("input").forEach((item) => {
     item.addEventListener("blur", (e) => {
-        currentFormAuth.login = document.getElementById('enterLogin')?.value;
-        currentFormAuth.password = document.getElementById('enterPassword')?.value;
+        currentFormAuth.login = document.getElementById("enterLogin")?.value;
+        currentFormAuth.password =
+            document.getElementById("enterPassword")?.value;
         showError.showError("login", "errorLogin", e, validator);
         showError.showError("password", "errorPassword", e, validator);
-        if (window.location.pathname === '/' || window.location.pathname === '/auth') {console.log('Текущие значения в форме: ', currentFormAuth)};
+        if (
+            window.location.pathname === "/" ||
+            window.location.pathname === "/auth"
+        ) {
+            console.log("Текущие значения в форме: ", currentFormAuth);
+        }
     });
 });
 template.querySelectorAll("input").forEach((item) => {
     item.removeEventListener("blur", () => {});
+});
+
+document.querySelector("form")?.addEventListener("click", (e) => {
+    // по клику генерируется submit
+    e.preventDefault();
+    e.stopPropagation();
+    template.querySelectorAll("input").forEach((item) => {
+        if (validator(item.name, item.value) && item.name === "password") {
+            document.getElementById("errorPassword").style.opacity = 1;
+        }
+        if (validator(item.name, item.value) && item.name === "login") {
+            document.getElementById("errorLogin").style.opacity = 1;
+        }
+    });
 });
--- src/pages/passwordChange/index.js
+++ src/pages/passwordChange/index.js
@@ -1,43 +1,44 @@
-import Button from '../../components/button/index';
-import Title from '../../components/title/index';
-import Input from '../../components/inputs/index';
-import Link from '../../components/link/index';
-import Ava from '../../components/ava/index';
-import Error from '../../components/error';
-import errorMsg from '../../data/errorMsg';
+import Button from "../../components/button/index";
+import Title from "../../components/title/index";
+import Input from "../../components/inputs/index";
+import Link from "../../components/link/index";
+import Ava from "../../components/ava/index";
+import Error from "../../components/error";
+import errorMsg from "../../data/errorMsg";
 import validations from "../../helpers/validation";
 import showError from "../../helpers/showError";
-import { log } from 'handlebars';
 
 function render(temp, arrBlock) {
     arrBlock.forEach((item) => {
         temp.appendChild(item.getContent());
-    })
+    });
     return temp;
-};
+}
 
-const template = document.getElementById("app");
+const form = document.createElement("form");
+form.id = "passwordForm";
+const template = document.getElementById("app").appendChild(form);
 
 const ava = new Ava({
-    class: "ava"
+    class: "ava",
 });
 const inputOldPsw = new Input({
     inputType: "password",
     inputName: "password",
     placeHolderText: "старый\u00A0пароль",
-    inputId: "password"
+    inputId: "password",
 });
 const inputNewPsw = new Input({
     inputType: "password",
     inputName: "passwordCopy",
     placeHolderText: "новый\u00A0пароль",
-    inputId: "passwordCopy"
+    inputId: "passwordCopy",
 });
 const inputNewPswCopy = new Input({
     inputType: "password",
     inputName: "passwordNewCopy",
     placeHolderText: "новый\u00A0пароль\u00A0(еще\u00A0раз)",
-    inputId: "passwordNewCopy"
+    inputId: "passwordNewCopy",
 });
 const errorPassword = new Error({
     message: errorMsg.messages.password,
@@ -52,59 +53,105 @@ const errorPasswordNewCopy = new Error({
     errorId: "errorPasswordNewCopy",
 });
 const title = new Title({
-    text: "Смена пароля"
+    text: "Смена пароля",
 });
 const buttonSave = new Button({
     class: "btn",
     type: "submit",
-    url: "/chating",
+    // url: "/chating",
     label: "Сохранить изменения",
     id: "saveBtn",
 });
 const buttonCancel = new Button({
     class: "btn",
-    type: "submit",
+    type: "button",
     url: "/",
     label: "Отмена",
     id: "cancelBtn",
 });
 const link = new Link({
-    url: '/',
-    title: 'Удалить аккаунт'
+    url: "/",
+    title: "Удалить аккаунт",
 });
 
-const result = [title, ava, inputOldPsw, errorPassword, inputNewPsw, errorPasswordCopy, inputNewPswCopy, errorPasswordNewCopy, buttonSave, buttonCancel, link];
+const result = [
+    title,
+    ava,
+    inputOldPsw,
+    errorPassword,
+    inputNewPsw,
+    errorPasswordCopy,
+    inputNewPswCopy,
+    errorPasswordNewCopy,
+    buttonSave,
+    buttonCancel,
+    link,
+];
 
 if (window.location.pathname === "/password-change") {
-    render(template, result)
+    console.log("/**/*/*", template);
+    render(template, result);
 }
 
 function validator(field, value) {
     if (field === "password") {
         return !validations.validations.password(value);
     } else if (field === "passwordCopy") {
-        return value !== document.getElementById('password').value;
+        return value !== document.getElementById("password").value;
     } else if (field === "passwordNewCopy") {
-        return value !== document.getElementById('passwordCopy').value;
-    } 
-};
+        return value !== document.getElementById("passwordCopy").value;
+    }
+}
 
 const currentFormPasswordChange = {
-    'password': '',
-    'passwordCopy': '',
-    'passwordNewCopy': '',
+    password: "",
+    passwordCopy: "",
+    passwordNewCopy: "",
 };
 template.querySelectorAll("input").forEach((item) => {
     item.addEventListener("blur", (e) => {
-        currentFormPasswordChange.password = document.getElementById('password')?.value;
-        currentFormPasswordChange.passwordCopy = document.getElementById('passwordCopy')?.value;
-        currentFormPasswordChange.passwordNewCopy = document.getElementById('passwordNewCopy')?.value;
+        currentFormPasswordChange.password =
+            document.getElementById("password")?.value;
+        currentFormPasswordChange.passwordCopy =
+            document.getElementById("passwordCopy")?.value;
+        currentFormPasswordChange.passwordNewCopy =
+            document.getElementById("passwordNewCopy")?.value;
         showError.showError("password", "errorPassword", e, validator);
         showError.showError("passwordCopy", "errorPasswordCopy", e, validator);
-        showError.showError("passwordNewCopy", "errorPasswordNewCopy", e, validator);
-        if (window.location.pathname === "/password-change") {console.log('Текущие значения в форме: ', currentFormPasswordChange)};
+        showError.showError(
+            "passwordNewCopy",
+            "errorPasswordNewCopy",
+            e,
+            validator
+        );
+        if (window.location.pathname === "/password-change") {
+            console.log(
+                "Текущие значения в форме: ",
+                currentFormPasswordChange
+            );
+        }
     });
 });
 template.querySelectorAll("input").forEach((item) => {
     item.removeEventListener("blur", () => {});
+});
+
+document.getElementById("passwordForm").addEventListener("click", (e) => {
+    // по клику генерируется submit
+    e.preventDefault();
+    e.stopPropagation();
+    template.querySelectorAll("input").forEach((item) => {
+        if (validator(item.name, item.value) && item.name === "password") {
+            document.getElementById("errorPassword").style.opacity = 1;
+        }
+        if (validator(item.name, item.value) && item.name === "passwordCopy") {
+            document.getElementById("errorPasswordCopy").style.opacity = 1;
+        }
+        if (
+            validator(item.name, item.value) &&
+            item.name === "passwordNewCopy"
+        ) {
+            document.getElementById("errorPasswordNewCopy").style.opacity = 1;
+        }
+    });
 });
