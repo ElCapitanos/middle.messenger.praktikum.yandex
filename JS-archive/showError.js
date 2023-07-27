474cc8de25e3020f58ecada4295a7777a4644b4f
Andrey Orlov <orlov75@gmail.com>
2023-07-27 19:27:40 +0300

    before final check-2

--- JS-archive/showError.js
+++
@@ -1,9 +0,0 @@
-function showError(field, errorElement, e, validator) {
-    if (document.getElementById(errorElement)) {
-        e.target.name === field && validator(field, e.target.value)
-            ? (document.getElementById(errorElement).style.opacity = 1)
-            : (document.getElementById(errorElement).style.opacity = 0);
-    }
-}
-
-export default { showError };
